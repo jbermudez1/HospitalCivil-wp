bab7ef1d4b8acee97def846cf9c913348a093352
diego <diego@proyeccione.com>
2016-08-09 17:29:57 -0600

    consultas

--- wp-content/themes/hcc/menus.php
+++ wp-content/themes/hcc/menus.php
@@ -103,21 +103,217 @@
                   <ul>
                     <li>
                       <span>
-                        <a href="#">Menu uno</a>
+                        <a href=" <?php echo esc_url( get_permalink(192) ); ?>">Alergología</a>
                       </span>
                     </li>
+                    <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(194) ); ?>">Anestesiología</a>
+                      </span>
+                    </li>
+                    <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(200) ); ?>">Angiología</a>
+                      </span>
+                    </li>
+                    <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(202) ); ?>">Biología de la Reproducción</a>
+                      </span>
+                    </li>
+                    <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(204) ); ?>">Cardiología</a>
+                      </span>
+                    </li>
+                      <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(206) ); ?>">Cirugía de Tórax</a>
+                      </span>
+                    </li>
+                      <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(208) ); ?>">Cirugía General</a>
+                      </span>
+                    </li>
+                      <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(210) ); ?>">Cirugía Pediátrica</a>
+                      </span>
+                    </li>
+                      <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(212) ); ?>">Cirugía Plástica</a>
+                      </span>
+                    </li>
+                      <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(214) ); ?>">Clínica del Dolor</a>
+                      </span>
+                    </li>
+                  
+                </div>
+                <div class="box">
+                  <ul>
+                    <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(216) ); ?>">Dermatología</a>
+                      </span>
+                    </li>
+                    <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(218) ); ?>">Endocrinólogo</a>
+                      </span>
+                    </li>
+                    <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(220) ); ?>">Gastroenterología</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(222) ); ?>">Geriatría</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(224) ); ?>">Ginecología</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(226) ); ?>">Gineco-Oncología</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(228) ); ?>">Hematología</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(230) ); ?>">Imagenología</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(232) ); ?>">Infectología</a>
+                      </span>
+                    </li>
+                    <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(234) ); ?>">Maxilofacial</a>
+                      </span>
+                    </li>
+        
+                  </ul>
+                </div>
+                <div class="box">
+                   <ul>
+                    <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(236) ); ?>">Medicina General</a>
+                      </span>
+                    </li>
+                    <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(238) ); ?>">Medicina Interna</a>
+                      </span>
+                    </li>
+                    <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(240) ); ?>">Medicina Materno Fetal</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(242) ); ?>">Nefrología</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(244) ); ?>">Neumología</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(246) ); ?>">Neurología</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(248) ); ?>">Neurocirugía</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(250) ); ?>">Nutrición</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(252) ); ?>">Oftalmología</a>
+                      </span>
+                    </li>
+                    <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(254) ); ?>">Oftalmopediatría</a>
+                      </span>
+                    </li>
+        
+                  </ul>
+                </div>
+                <div class="box">
+                   <ul>
+                    <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(256) ); ?>">Otorrinolaringología Otoneuro</a>
+                      </span>
+                    </li>
+                    <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(258) ); ?>">Pediatría</a>
+                      </span>
+                    </li>
+                    <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(260) ); ?>">Proctología</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(262) ); ?>">Psicología</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(264) ); ?>">Psiquiatría</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(266) ); ?>">Reumatología</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(269) ); ?>">Traumatología y Ortopedia</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(271) ); ?>">Urología</a>
+                      </span>
+                    </li>
+                       <li>
+                      <span>
+                        <a href=" <?php echo esc_url( get_permalink(273) ); ?>">Uro-ginecología</a>
+                      </span>
+                    </li> 
                   </ul>
                   s
                 </div>
-                <div class="box">
-                  s
-                </div>
-                <div class="box">
-                  s
-                </div>
-                <div class="box">
-                  s
-                </div>
+                </ul>
               </div>
             </div>
           </div>
